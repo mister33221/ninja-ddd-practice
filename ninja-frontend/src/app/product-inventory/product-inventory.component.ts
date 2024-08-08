3f72902f833406615b45e45cfddacc68d40f6af4
Kai <mister33221@gmail.com>
2024-08-08 21:15:07 +0800

    chore: Update product-inventory

--- ninja-frontend/src/app/product-inventory/product-inventory.component.ts
+++ ninja-frontend/src/app/product-inventory/product-inventory.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Offcanvas } from 'bootstrap';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 interface Product {
   id: number;
@@ -17,24 +19,32 @@ interface Product {
   styleUrls: ['./product-inventory.component.scss']
 })
 export class ProductInventoryComponent implements OnInit {
+  @ViewChild('offcanvas') offcanvasElement!: ElementRef;
+  private offcanvasInstance?: Offcanvas;
+  newImageUrl: SafeUrl | null = null;
+
   products: Product[] = [
     { id: 1, name: '商品 1', image: 'https://picsum.photos/200/200?random=1', stockQuantity: 100, safetyStockLevel: 20, description: '商品 1 的詳細描述', specifications: '商品 1 的規格' },
     { id: 2, name: '商品 2', image: 'https://picsum.photos/200/200?random=2', stockQuantity: 50, safetyStockLevel: 10, description: '商品 2 的詳細描述', specifications: '商品 2 的規格' },
     { id: 3, name: '商品 3', image: 'https://picsum.photos/200/200?random=3', stockQuantity: 75, safetyStockLevel: 15, description: '商品 3 的詳細描述', specifications: '商品 3 的規格' },
   ];
 
-  modalRef?: BsModalRef;
-  selectedProduct: Product | null = null;
+  selectedProduct: Product | any = null;
   isEditing: boolean = false;
 
-  constructor(private modalService: BsModalService) {}
+  constructor(private sanitizer: DomSanitizer) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
-  openModal(template: TemplateRef<any>, product: Product): void {
+  ngAfterViewInit() {
+    this.offcanvasInstance = new Offcanvas(this.offcanvasElement.nativeElement);
+  }
+
+  openOffcanvas(product: Product): void {
     this.selectedProduct = { ...product };
     this.isEditing = false;
-    this.modalRef = this.modalService.show(template, { class: 'modal-lg' });
+    this.offcanvasInstance?.show();
   }
 
   toggleEdit(): void {
@@ -51,11 +61,24 @@ export class ProductInventoryComponent implements OnInit {
     }
   }
 
-  closeModal(): void {
-    this.modalRef?.hide();
+  closeOffcanvas(): void {
+    this.offcanvasInstance?.hide();
+    this.selectedProduct = null;
   }
 
   isLowStock(product: Product): boolean {
     return product.stockQuantity <= product.safetyStockLevel;
+  }
+
+  onFileChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if (target.files && target.files.length > 0) {
+      const file = target.files[0];
+      const objectUrl = URL.createObjectURL(file);
+      this.newImageUrl = this.sanitizer.bypassSecurityTrustUrl(objectUrl);
+      if (this.selectedProduct) {
+        this.selectedProduct.image = this.newImageUrl;
+      }
+    }
   }
 }
