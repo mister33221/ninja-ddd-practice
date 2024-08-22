cf93e8b876a8f93be921225e63c8141f1467be03
Kai <mister33221@gmail.com>
2024-08-22 22:53:03 +0800

    test: Test render

--- ninja-frontend/src/app/app.component.ts
+++ ninja-frontend/src/app/app.component.ts
@@ -4,19 +4,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = '木葉村忍具店';
   isLoggedIn = false; // 這應該根據實際的登錄狀態來設置
   userName = '鳴人'; // 這應該是實際登錄用戶的名字
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  test(){
-    this.httpClient.get('https://ninja-backend.onrender.com/hello/world').subscribe((res) => {
-      alert(res);
-    });
+  // https://ninja-backend.onrender.com/hello/world
+  test() {
+    this.httpClient
+      .get('https://ninja-backend.onrender.com/hello/world', {
+        responseType: 'text',
+      })
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          alert(response);
+        },
+        error: (error) => {
+          console.error(error);
+        },
+      });
   }
-
 }
